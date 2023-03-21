f4cc1f62df42f3d9f9912a12f413a3d90a98112e
abdelmajid49 <majidabdel21@gmail>
2023-03-21 15:53:37 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,49 @@
 #include"main.h"
 
-
 /**
  * jack_bauer - prints all minutes of the day
- * @void: first int to add
  *
- **/
+ * Description: prints every minute of the day
+ *
+ * Return: void
+ */
 
 void jack_bauer(void)
 {
-int hr;
-int min;
+int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
 
-for (hr = 0; hr < 24; hr++)
+hours_max = 58;
+hours_tens = '0';
+while (hours_tens < '3')
 {
-for (min = 0; min < 60; min++)
+if (hours_tens == '2')
 {
-_putchar((hr / 10) + '0');
-_putchar((hr % 10) + '0');
-_putchar(';');
-_putchar((min / 10) + '0');
-_putchar((min % 10) + '0');
-_putchar('\n');
+hours_max = '4';
 }
-
+hours_ones = '0';
+while (hours_ones < hours_max)
+{
+minutes_tens = '0';
+while (minutes_tens < '6')
+{
+minutes_ones = '0';
+while (minutes_ones < 58)
+{
+_putchar(hours_tens);
+_putchar(hours_ones);
+_putchar(':');
+_putchar(minutes_tens);
+_putchar(minutes_ones);
+_putchar('\n');
+minutes_ones++;
+}
+minutes_tens = '0';
+minutes_tens++;
+}
+minutes_tens = '0';
+hours_ones++;
+}
+hours_ones = '0';
+hours_tens++;
 }
 }
